d99f9129118d5942381c1e14bdee6012a8f1dcb6
Revasfr <revamox@gmail.com>
2023-01-10 10:46:59 +0700

    intial commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>VOGUE</title>
+  </head>
+  <body>
+  <div class="card text-center ">
+      <h1>Welcome to Vogue Cafe!</h1>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">VOGUE'S</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/beranda">Beranda</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/menu">Vogue Menu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/walas" tabindex="-1" aria-disabled="true"></a>
+              </li>
+            </ul>
+            <li>
+              <a href="#"><img src="voguecaf.png" width="40" heigth="40"></a>
+            </li>
+            <!-- <form class="d-flex">
+            <a href="/login" class="btn btn-primary float-end">Login</a>
+            </form> -->
+          </div>
+        </div>
+     </nav>
+
+     <div class="container mt-3">
+      <div class="card text-center mt-5">
+        <h2>Food for the Fashionable</h2> 
+      </div>
+
+      <div class="card mb-3">
+  <img src="cafe.jpeg" class="card-img-top" alt="gambar_cafe" width="400" height="350">
+  <div class="card-body">
+    <h3 class="card-title">Coffe</h3>
+    <p class="card-text">The legal version of crack. Liquid gold. It addition to food and water, clothing, and shelter, coffe is the new crucical substance needed for the survival of the human race.</p>
+    <p class="card-text">[1] The most important meal of the day.<br>
+                         [2] Isn't a drop - it's a vitamin.<br>
+                         [3] Behind every succesful person is a substantial amount of coffe.<br>
+                         [4] Sometime I like coffe more than I like people</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+
+      <nav class="navbar fixed-bottom navbar-dark bg-dark mt-5">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">@VOGUE CAFE</a>
+        <a class="navbar-brand" href="#">&#169REVA</a>
+        </div>
+    </nav>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
